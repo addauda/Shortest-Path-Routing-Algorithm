5eedb6164b855ae49c5bc38a9d0a3bab7bc2390b
Ahmed Dauda <ahmed.dauda@uoit.net>
2020-04-01 11:40:20 -0400

    added router name to start script

--- router.sh
+++ router.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -gt 0 ]; then
-    echo "Router started with $# arguments"
+    echo "R$1 started with $# arguments"
     node router.js $1 $2 $3 $4
 else
     echo "Please provide required parameters"
